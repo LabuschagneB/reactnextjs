abef9807e4c7d68d8f5a17fcda509a563c786688
Bryan Labuschagne <bryanlabuschagne@gmail.com>
2026-03-14 21:44:25 -0700

    add vercel link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import '@/app/ui/global.css';
 import { ubuntu } from '@/app/ui/fonts';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <SpeedInsights/>
       <body className={`${ubuntu.className} antialiased`}>{children}</body>
     </html>
   );
