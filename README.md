b0ccf154b4fc918f01d1dbd86d85ffd8b921bd82
Bryan Labuschagne <bryanlabuschagne@gmail.com>
2026-03-20 23:20:01 -0700

    remove all

--- README.md
+++
@@ -1,3 +0,0 @@
-## My profile made with Next.Js and React.Js
-
-You can view this project on my Vercel url [here](https://yourwebsite.com).
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <main className="flex items-center justify-center h-screen bg-gray-50">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello my name is Bryan!
-      </h1>
-    </main>
-  )
-}
